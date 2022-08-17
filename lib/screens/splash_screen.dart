48c3308227b9f4f78a9411d2e8953b1e4ac8e260
nataliianovykova <nataliianovykova@gmail.com>
2022-08-17 14:56:02 +0200

    Add privacy policy and terms of use

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -3,11 +3,13 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:hello_flutter/utils/constants.dart';
 import 'package:lottie/lottie.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../ads/AdOpen.dart';
 import '../lifecycles/extentions.dart';
 import '../lifecycles/orientation.dart';
 import '../utils/logging.dart';
+import 'package:flutter/gestures.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -16,7 +18,8 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> with WidgetsBindingObserver {
+class _SplashScreenState extends State<SplashScreen>
+    with WidgetsBindingObserver {
   late bool _loading;
   late double _progressValue;
   final maxSplashTime = 10000;
@@ -46,69 +49,108 @@ class _SplashScreenState extends State<SplashScreen> with WidgetsBindingObserver
   @override
   Widget build(BuildContext context) {
     return Container(
-        color: Colors.pink[200],
-        // width: 360, // my Xiaomi Mi A3 points
-        // height: 732, // my Xiaomi Mi A3 points
-        alignment: Alignment.center,
-        padding: const EdgeInsets.only(top: 100),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            SizedBox(
-              height: 250,
-              width: 250,
-              child: Lottie.asset("assets/lottie/monkey.json"),
-              // child: Lottie.network("https://assets7.lottiefiles.com/packages/lf20_bburfggv.json"),
-              // child: Image(image: AssetImage('assets/imgs/darth_vader.png')),
+      color: Colors.pink[200],
+      // width: 360, // my Xiaomi Mi A3 points
+      // height: 732, // my Xiaomi Mi A3 points
+      alignment: Alignment.center,
+      padding: const EdgeInsets.only(top: 100),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          SizedBox(
+            height: 250,
+            width: 250,
+            child: Lottie.asset("assets/lottie/monkey.json"),
+            // child: Lottie.network("https://assets7.lottiefiles.com/packages/lf20_bburfggv.json"),
+            // child: Image(image: AssetImage('assets/imgs/darth_vader.png')),
+          ),
+          const Text(
+            "Flutter Monkey Cleaner",
+            style: TextStyle(
+              decoration: TextDecoration.none,
+              color: Colors.black54,
+              fontSize: 32.0,
+              letterSpacing: 4,
+              fontFamily: "MouseMemoirs",
             ),
-            const Text(
-              "Flutter Monkey Cleaner",
-              style: TextStyle(
-                decoration: TextDecoration.none,
-                color: Colors.black54,
-                fontSize: 32.0,
-                letterSpacing: 4,
-                fontFamily: "MouseMemoirs",
+          ),
+          Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.all(20),
+            child: Center(
+                child: DefaultTextStyle(
+              style: const TextStyle(),
+                child: Text.rich(
+                    textAlign: TextAlign.center,
+                    TextSpan(
+                        text:
+                            'By proceeding, you confirm that you have read and acknowledged ',
+                        style: const TextStyle(
+                          fontSize: 16,
+                          color: Colors.black87,
+                          fontFamily: "CustomIcons.ttf",
+                        ),
+                        children: <TextSpan>[
+                          TextSpan(
+                              text: 'Terms of Service',
+                              style: const TextStyle(
+                                fontSize: 16,
+                                color: Colors.blueAccent,
+                                decoration: TextDecoration.underline,
+                              ),
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = () {
+                                  launchUrl(Uri.parse(urlPolicy));
+                                }),
+                          TextSpan(
+                              text: ' and ',
+                              style: const TextStyle(
+                                  fontSize: 16, color: Colors.black87),
+                              children: <TextSpan>[
+                                TextSpan(
+                                    text: 'Privacy Policy',
+                                    style: const TextStyle(
+                                        fontSize: 16,
+                                        color: Colors.blueAccent,
+                                        decoration: TextDecoration.underline),
+                                    recognizer: TapGestureRecognizer()
+                                      ..onTap = () {
+                                        launchUrl(Uri.parse(urlTerms));
+                                      }),
+                              ]),
+                        ])),
+
+            )),
+          ),
+          Visibility(
+            visible: _loading,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: LinearProgressIndicator(value: _progressValue),
+            ),
+          ),
+          Visibility(
+            // visible: !_loading,
+            child: TextButton(
+              onPressed: () => {
+                setState(() {
+                  _loading = !_loading;
+                  _startProgress();
+                  AdOpen.load(() {
+                    speedProgress = 4.0;
+                  });
+                }),
+              },
+              child: const Text(
+                "START",
+                textScaleFactor: 2.4,
               ),
             ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: Text(
-                "By proceeding, you confirm that you have read and acknowledged Privacy Policy and Terms of Service",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  decoration: TextDecoration.none,
-                  color: Colors.black54,
-                  fontSize: 14.0,
-                ),
-              ),
-            ),
-            Visibility(
-              visible: _loading,
-              child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: LinearProgressIndicator(value: _progressValue)),
-            ),
-            Visibility(
-              // visible: !_loading,
-              child: TextButton(
-                  onPressed: () => {
-                        setState(() {
-                          _loading = !_loading;
-                          _startProgress();
-                          AdOpen.load(() {
-                            speedProgress = 4.0;
-                          });
-                        })
-                      },
-                  child: const Text(
-                    "START",
-                    textScaleFactor: 2.4,
-                  )),
-            ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
   }
 
   void _startProgress() {
@@ -120,7 +162,8 @@ class _SplashScreenState extends State<SplashScreen> with WidgetsBindingObserver
           timer.cancel();
           launchWhenResumed(() {
             // pushReplacementNamed - навигация без возврата бекпрессом
-            AdOpen.show(() => {Navigator.pushReplacementNamed(context, homeScreen)});
+            AdOpen.show(
+                () => {Navigator.pushReplacementNamed(context, homeScreen)});
           });
           return;
         }
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,3 +1,6 @@
 
 const String splashScreen = "SplashScreen";
 const String homeScreen = "HomeScreen";
+
+const String urlPolicy = 'https://docs.flutter.io/flutter/services/UrlLauncher-class.html';
+const String urlTerms = 'https://docs.flutter.io/flutter/services/UrlLauncher-class.html';
