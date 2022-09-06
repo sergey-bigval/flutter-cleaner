236cf3fea414b3951490e3607f5c3a2c41006600
olegpetskovich <OlegPetskovich@gmail.com>
2022-09-06 15:10:26 +0300

    Refactored BLOC code

--- lib/screens/device_info/bloc/device_info_bloc.dart
+++ lib/screens/device_info/bloc/device_info_bloc.dart
@@ -1,26 +1,34 @@
 import 'dart:async';
 
 import 'package:battery_plus/battery_plus.dart';
+import 'package:disk_space/disk_space.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:hello_flutter/utils/logging.dart';
+import 'package:system_info/system_info.dart';
 
 class DeviceInfoBloc extends Bloc<DeviceInfoEvent, DeviceInfoState> {
   DeviceInfoBloc() : super(DeviceInfoState.initial()) {
     on<InfoBatteryLevel>(_onInfoBatteryLevel);
-      on<_InfoBatteryLevel>(_onInnerInfoBatteryLevel);
+      on<_InfoBatteryLevel>(_onPrivateInfoBatteryLevel);
 
     on<InfoBatteryState>(_onInfoBatteryState);
+      on<_InfoBatteryState>(_onPrivateInfoBatteryState);
+
     on<InfoBatteryIsSaveMode>(_onInfoBatteryIsSaveMode);
+
+    on<InfoMemory>(_onMemoryData);
+    on<InfoRam>(_onRamData);
   }
 
-  Future<void> _onInfoBatteryLevel(InfoBatteryLevel event, Emitter emitter) async {
+  Future<void> _onInfoBatteryLevel(
+      InfoBatteryLevel event, Emitter emitter) async {
     Timer.periodic(const Duration(seconds: 5), (timer) async {
       add(_InfoBatteryLevel(batteryLevel: await Battery().batteryLevel));
     });
   }
 
-  Future<void> _onInnerInfoBatteryLevel(_InfoBatteryLevel event, Emitter emitter) async {
-      emitter(state.copyWith(batteryLevel: event.batteryLevel));
+  Future<void> _onPrivateInfoBatteryLevel(
+      _InfoBatteryLevel event, Emitter emitter) async {
+    emitter(state.copyWith(batteryLevel: event.batteryLevel));
   }
 
   Future<void> _onInfoBatteryState(
@@ -51,11 +59,13 @@ class DeviceInfoBloc extends Bloc<DeviceInfoEvent, DeviceInfoState> {
             break;
           }
       }
-
-      lol("STATE КФКФКФКФ");
-
+      add(_InfoBatteryState(batteryState: batteryState));
     });
-    emitter(state.copyWith(batteryState: batteryState));
+  }
+
+  Future<void> _onPrivateInfoBatteryState(
+      _InfoBatteryState event, Emitter emitter) async {
+    emitter(state.copyWith(batteryState: event.batteryState));
   }
 
   Future<void> _onInfoBatteryIsSaveMode(
@@ -72,42 +82,96 @@ class DeviceInfoBloc extends Bloc<DeviceInfoEvent, DeviceInfoState> {
       emitter(state.copyWith(batteryIsSaveMode: saveModeMessage));
     });
   }
+
+  Future<void> _onMemoryData(
+    InfoMemory event,
+    Emitter emitter,
+  ) async {
+    var totalMemory = await DiskSpace.getTotalDiskSpace.then((value) => value! / 1024);
+    var freeMemory = await DiskSpace.getFreeDiskSpace.then((value) => value! / 1024);
+    emitter(state.copyWith(totalMemory: totalMemory, freeMemory: freeMemory));
+  }
+
+  Future<void> _onRamData(
+    InfoRam event,
+    Emitter emitter,
+  ) async {
+    emitter(state.copyWith(
+        totalRam: (SysInfo.getTotalPhysicalMemory() ~/ (1024 * 1024)).toDouble(),
+        freeRam: (SysInfo.getFreePhysicalMemory() ~/ (1024 * 1024)).toDouble()));
+  }
 }
 
 abstract class DeviceInfoEvent {}
 
 class InfoBatteryLevel extends DeviceInfoEvent {}
+
 class _InfoBatteryLevel extends DeviceInfoEvent {
   final int batteryLevel;
 
   _InfoBatteryLevel({required this.batteryLevel});
 }
 
-
 class InfoBatteryState extends DeviceInfoEvent {}
+
+class _InfoBatteryState extends DeviceInfoEvent {
+  final String batteryState;
+
+  _InfoBatteryState({required this.batteryState});
+}
+
 class InfoBatteryIsSaveMode extends DeviceInfoEvent {}
+
+class InfoMemory extends DeviceInfoEvent {}
+class InfoRam extends DeviceInfoEvent {}
 
 class DeviceInfoState {
   final int batteryLevel;
   final String batteryState;
   final String batteryIsSaveMode;
 
+  final double totalMemory;
+  final double freeMemory;
+
+  final double totalRam;
+  final double freeRam;
+
   DeviceInfoState(
       {required this.batteryLevel,
       required this.batteryState,
-      required this.batteryIsSaveMode});
+      required this.batteryIsSaveMode,
+      required this.totalMemory,
+      required this.freeMemory,
+      required this.totalRam,
+      required this.freeRam});
 
   factory DeviceInfoState.initial() => DeviceInfoState(
-      batteryLevel: 0,
-      batteryState: "Not available",
-      batteryIsSaveMode: "disabled");
+        batteryLevel: 0,
+        batteryState: "Not available",
+        batteryIsSaveMode: "disabled",
+        totalMemory: 0,
+        freeMemory: 0,
+        totalRam: 0,
+        freeRam: 0,
+      );
 
-  DeviceInfoState copyWith(
-      {int? batteryLevel, String? batteryState, String? batteryIsSaveMode}) {
+  DeviceInfoState copyWith({
+    int? batteryLevel,
+    String? batteryState,
+    String? batteryIsSaveMode,
+    double? totalMemory,
+    double? freeMemory,
+    double? totalRam,
+    double? freeRam,
+  }) {
     return DeviceInfoState(
       batteryLevel: batteryLevel ?? this.batteryLevel,
       batteryState: batteryState ?? this.batteryState,
       batteryIsSaveMode: batteryIsSaveMode ?? this.batteryIsSaveMode,
+      totalMemory: totalMemory ?? this.totalMemory,
+      freeMemory: freeMemory ?? this.freeMemory,
+      totalRam: totalRam ?? this.totalRam,
+      freeRam: freeRam ?? this.freeRam,
     );
   }
 }
--- lib/screens/device_info/device_info.dart
+++ lib/screens/device_info/device_info.dart
@@ -1,16 +1,9 @@
-import 'dart:async';
-import 'dart:io';
-
-import 'package:battery_plus/battery_plus.dart';
-import 'package:disk_space/disk_space.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hello_flutter/screens/device_info/widgets/app_bar.dart';
 import 'package:hello_flutter/screens/device_info/widgets/battery_text.dart';
 import 'package:hello_flutter/screens/device_info/widgets/memory_card.dart';
 import 'package:hello_flutter/utils/logging.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:system_info/system_info.dart';
 
 import 'bloc/device_info_bloc.dart';
 
@@ -22,23 +15,12 @@ class BatteryInfoPage extends StatefulWidget {
 }
 
 class DeviceInfoPageState extends State<BatteryInfoPage> {
-  StreamSubscription<BatteryState>? _batteryStateSubscription;
-
   final mainTextStyleSmall = const TextStyle(
     decoration: TextDecoration.none,
     color: Colors.black54,
     fontSize: 14.0,
     letterSpacing: 1,
   );
-
-  double _diskSpace = 0;
-  Map<Directory, double> _directorySpace = {};
-
-  double totalDiskSpace = 0;
-  double freeDiskSpace = 0;
-
-  int totalRam = SysInfo.getTotalPhysicalMemory() ~/ (1024 * 1024);
-  int freeRam = SysInfo.getFreePhysicalMemory() ~/ (1024 * 1024);
 
   late final DeviceInfoBloc _bloc;
 
@@ -50,6 +32,8 @@ class DeviceInfoPageState extends State<BatteryInfoPage> {
     _bloc.add(InfoBatteryLevel());
     _bloc.add(InfoBatteryState());
     _bloc.add(InfoBatteryIsSaveMode());
+    _bloc.add(InfoMemory());
+    _bloc.add(InfoRam());
   }
 
   @override
@@ -124,21 +108,33 @@ class DeviceInfoPageState extends State<BatteryInfoPage> {
             ),
             Row(
               children: [
-                MemoryCard(
-                  cardColor: Colors.deepPurpleAccent,
-                  iconColor: Colors.deepPurpleAccent.shade100,
-                  icon: Icons.sd_storage_rounded,
-                  totalMemory: totalDiskSpace,
-                  freeMemory: freeDiskSpace,
-                  unit: "GB",
+                BlocConsumer<DeviceInfoBloc, DeviceInfoState>(
+                  bloc: _bloc,
+                  listener: (context, state) {},
+                  builder: (context, state) {
+                    return MemoryCard(
+                      cardColor: Colors.deepPurpleAccent,
+                      iconColor: Colors.deepPurpleAccent.shade100,
+                      icon: Icons.sd_storage_rounded,
+                      totalMemory: state.totalMemory,
+                      freeMemory: state.freeMemory,
+                      unit: "GB",
+                    );
+                  },
                 ),
-                MemoryCard(
-                  cardColor: Colors.orangeAccent.shade200,
-                  iconColor: Colors.deepOrangeAccent,
-                  icon: Icons.memory_rounded,
-                  totalMemory: totalRam.toDouble(),
-                  freeMemory: freeRam.toDouble(),
-                  unit: "MB",
+                BlocConsumer<DeviceInfoBloc, DeviceInfoState>(
+                  bloc: _bloc,
+                  listener: (context, state) {},
+                  builder: (context, state) {
+                    return MemoryCard(
+                      cardColor: Colors.orangeAccent.shade200,
+                      iconColor: Colors.deepOrangeAccent,
+                      icon: Icons.memory_rounded,
+                      totalMemory: state.totalRam,
+                      freeMemory: state.freeRam,
+                      unit: "MB",
+                    );
+                  },
                 ),
               ],
             )
@@ -146,52 +142,5 @@ class DeviceInfoPageState extends State<BatteryInfoPage> {
         ),
       ),
     );
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    if (_batteryStateSubscription != null) {
-      _batteryStateSubscription!.cancel();
-    }
-  }
-
-  void initDiskSpace() async {
-    double? diskSpace = 0;
-
-    diskSpace = await DiskSpace.getFreeDiskSpace;
-
-    List<Directory> directories;
-    Map<Directory, double> directorySpace = {};
-
-    if (Platform.isIOS) {
-      directories = [await getApplicationDocumentsDirectory()];
-    } else if (Platform.isAndroid) {
-      directories =
-          await getExternalStorageDirectories(type: StorageDirectory.movies)
-              .then(
-        (list) async => list ?? [await getApplicationDocumentsDirectory()],
-      );
-    } else {
-      return;
-    }
-
-    for (var directory in directories) {
-      var space = await DiskSpace.getFreeDiskSpaceForPath(directory.path);
-      directorySpace.addEntries([MapEntry(directory, space!)]);
-    }
-
-    if (!mounted) return;
-
-    setState(() {
-      _diskSpace = diskSpace!;
-      _directorySpace = directorySpace;
-      lol("disc SPACE = ${diskSpace / 1024}");
-      lol("directory SPACE = $directorySpace");
-      // values are in GB
-      DiskSpace.getFreeDiskSpace.then((value) => freeDiskSpace = value! / 1024);
-      DiskSpace.getTotalDiskSpace
-          .then((value) => totalDiskSpace = value! / 1024);
-    });
   }
 }
